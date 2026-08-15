7183218d57745ff4ec337f34d168253f7919ded6
Rahul Somasunderam <rahul.som@gmail.com>
2026-08-15 16:08:40 -0700

    chore: Use io.github to start groupId
    Gradle rejects com.github

--- plugin/src/test/kotlin/com/github/rahulsom/RoseauPluginTest.kt
+++ plugin/src/test/kotlin/com/github/rahulsom/RoseauPluginTest.kt
@@ -8,7 +8,7 @@ class RoseauPluginTest {
     @Test fun `plugin registers task`() {
         // Create a test project and apply the plugin
         val project = ProjectBuilder.builder().build()
-        project.plugins.apply("com.github.rahulsom.roseau")
+        project.plugins.apply("io.github.rahulsom.roseau")
         project.plugins.apply("java")
 
         // Verify the result
